b800a46b29f4c7f622112f02b973ea9d4406fd74
Gabriel988 <gabriel@jltecno.com.br>
2022-03-21 14:59:28 -0300

    ajustes

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 
